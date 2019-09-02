de19ecc901de84fd4ab9ae8e0a5d123bb91008dc
LyMarco <ly_marco@yahoo.ca>
2019-09-02 08:21:38 -0400

    hotfix weather updates from 30 seconds to 5 minutes

--- index.js
+++ index.js
@@ -360,7 +360,7 @@ function startWeatherChecks(bot) {
     var weatherInterval = setInterval(async function() {
         // console.log('weatherInterval', new Date(), currentWeather);
         checkWeatherChange(bot);
-    }, '30000');
+    }, '300000');
     return weatherInterval;
 }
 
